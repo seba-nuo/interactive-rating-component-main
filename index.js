2ea10629544fc82d78ae3ae38536b83c7381ca6f
seba <sebas.a.nunez@gmail.com>
2022-05-06 20:45:35 -0500

    update to persist selected rate

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-let lastRateClicked = null
+const rateButtons = document.querySelector(".rate").querySelectorAll("button");
+let lastRateClicked = null;
 
 function updateLastRate(e) {
-  const rate = document.querySelector('#selected')
-  lastRateClicked = e.target.id
-  rate.innerText = lastRateClicked
+  rateButtons.forEach((btn) => {
+    btn.classList.remove("selected"); // remove all selected class
+  });
+  e.target.classList.add("selected"); //add selected class to the target
+
+  const rate = document.querySelector("#selected");
+  lastRateClicked = e.target.id;
+  rate.innerText = lastRateClicked;
 }
 
 function handleSubmit(e) {
-  e.preventDefault()
-  if (lastRateClicked == null) return
-  const ratingForm = document.querySelector('.form')
-  const thankYou = document.querySelector('.thank-you')
-  ratingForm.classList.toggle('none')
-  thankYou.classList.toggle('none')
+  e.preventDefault();
+  if (lastRateClicked == null) return;
+  const ratingForm = document.querySelector(".form");
+  const thankYou = document.querySelector(".thank-you");
+  ratingForm.classList.toggle("none");
+  thankYou.classList.toggle("none");
 }
